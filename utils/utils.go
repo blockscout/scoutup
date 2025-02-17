37cc677e5597aae3d91875e7c7a553108d62fccf
Rim Rakhimov <rimrakhimov@gmail.com>
2025-02-17 14:05:12 +0400

    feat: verify predeployed interop contracts

--- utils/utils.go
+++ utils/utils.go
@@ -5,6 +5,13 @@ import (
 	"strings"
 
 	"github.com/joho/godotenv"
+
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"io"
+	"net/http"
 )
 
 func PatchDotEnv(path string, envs map[string]string) error {
@@ -37,4 +44,46 @@ func NameToContainerName(prefix string, name string) string {
 	container_name := strings.ToLower(name)
 	container_name = strings.ReplaceAll(container_name, " ", "-")
 	return prefix + "-" + container_name
+}
+
+func GetSmartContract(backendURL string, address common.Address) ([]byte, error) {
+	url := fmt.Sprintf("%s/api/v2/smart-contracts/%s", backendURL, address)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(fmt.Sprintf("invalid response status code: %s", resp.Status))
+	}
+
+	return io.ReadAll(resp.Body)
+}
+
+func RetrieveProxyImplementationAddresses(backendURL string, proxy common.Address) ([]common.Address, error) {
+	body, err := GetSmartContract(backendURL, proxy)
+	if err != nil {
+		return nil, err
+	}
+
+	type Implementation struct {
+		Address common.Address `json:"address"`
+	}
+
+	type Response struct {
+		Implementations []Implementation `json:"implementations"`
+	}
+
+	var data Response
+	if err := json.Unmarshal(body, &data); err != nil {
+		return nil, err
+	}
+
+	var addresses []common.Address
+	for _, implementation := range data.Implementations {
+		addresses = append(addresses, implementation.Address)
+	}
+	return addresses, nil
 }
